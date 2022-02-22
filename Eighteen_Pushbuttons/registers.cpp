9ced4b285bcb802333d4bf4fad57f85ced392719
David <david@Davids-MacBook-Air-3.local>
2022-02-21 20:51:29 -0800

    overlay registers with structs

--- Eighteen_Pushbuttons/registers.cpp
+++ Eighteen_Pushbuttons/registers.cpp
@@ -3,22 +3,22 @@
 
 char bitChar(byte * regAddress, byte bitNumber) {
   char bitDigit[] = {'0', '1'};
-  // handle ACSR differently 
+  // handle ACSR differently from digital pins
   // pin grounded > ACO = 1 = button down
-  // so we use the ACO bit directly
   if (regAddress == &ACSR) {      
     if (acsr.aco) {
-      return '1';
+      return '1'; // when pin is HIGH
     } else {
-      return '0';
+      return '0'; // when pin is LOW
     }  
   } else {
-    // other pins, ground = LOW
+    // digital pins 
+    // ground = LOW = 0 = button down
     // so we reverse the indication 
     if (*regAddress & (1 << bitNumber)) {
-      return '0';
+      return '0'; // when pin is HIGH
     } else {
-      return '1';
+      return '1'; // when pin is LOW
     }  
     
   }
@@ -26,29 +26,26 @@ char bitChar(byte * regAddress, byte bitNumber) {
 
 void activateAnalogComparator() {
   // enable the Analogue Comparator
+  // five actions are required, in the following order
+  
   // write 0 to the bit ACIE
   // in the Analogue Comparator Control and Storage Register ACSR
-  // Note: use NOT-AND logic to clear the bit
-  // ACSR &= ~(0x01 << ACIE);
   acsr.acie = 0;
 
   // Select Internal Bandgap Reference Voltage
-  // write 1 to the ACBG bit
-  // in the ACSR register
-  // use OR logic to set the bit
-  // ACSR |= (0x01 << ACBG);
+  // write 1 to the ACBG bit in the ACSR register
   acsr.acbg = 1;
   // arbitrary pause to allow the Bandgap voltage to stabilize
   delay(100);
 
   // turn on the ADC by clearing bit PRADC in register PRR  
-  PRR &= ~(0x01 << PRADC);
+  prr.pradc = 0;
 
   // disable ADC using the ADC multiplexor
   // by clearing bit ADEN in register ADCSRA  
-  ADCSRA &= ~(0x01 << ADEN);
+  adcsra.aden = 0;
 
   // enable the comparator to use the ADC multiplexor
   // by setting bit ACME in register ADCSRB  
-  ADCSRB |= (0x01 << ACME);  
+  adcsrb.acme = 1;
 }
--- Eighteen_Pushbuttons/registers.h
+++ Eighteen_Pushbuttons/registers.h
@@ -5,16 +5,55 @@
 #ifndef REGISTERS_H
 #define REGISTERS_H
 
+struct regPRR {
+  // PRR register at 9.11.3 pg 38
+  uint8_t pradc:1;    // Bit 0 - PRADC: Power Reduction ADC
+  uint8_t prusart0:1; // Bit 1 - PRUSART0: Power Reduction USART0
+  uint8_t prspi:1;    // Bit 2 - PRSPI: Power Reduction Serial Peripheral Interface
+  uint8_t prtim1:1;   // Bit 3 - PRTIM1: Power Reduction Timer/Counter1
+  uint8_t reserved:1; // Bit 4 - Res: Reserved bit will always read as zero
+  uint8_t prtim0:1;   // Bit 5 - PRTIM0: Power Reduction Timer/Counter0
+  uint8_t prtim2:1;   // Bit 6 - PRTIM2: Power Reduction Timer/Counter2
+  uint8_t prtwi:1;    // Bit 7 - PRTWI: Power Reduction TWI
+};
+#define prr (*(volatile regPRR *)(&PRR))
+
+struct regADCSRA {
+  // ADCSRA register at 23.9.2 pg 218
+  uint8_t adps:3;   // Bits 2:0 – ADPS2:0: ADC Prescaler Select Bits pg 219
+  uint8_t adie:1;   // Bit 3 – ADIE: ADC Interrupt Enable
+  uint8_t adif:1;   // Bit 4 – ADIF: ADC Interrupt Flag
+  uint8_t adate:1;  // Bit 5 – ADATE: ADC Auto Trigger Enable
+  uint8_t adsc:1;   // Bit 6 – ADSC: ADC Start Conversion
+  uint8_t aden:1;   // Bit 7 – ADEN: ADC Enable see pg 202
+};
+#define adcsra (*(volatile regADCSRA *)(&ADCSRA))
+
+struct regADCSRB {
+  // ADCSRB register at 23.9.4 pg 220
+  uint8_t adts:3;       // Bit 2:0 – ADTS2:0: ADC Auto Trigger Source
+  uint8_t reserved35:3; // Bit 5:3 Reserved
+                        // To ensure compatibility with future devices, 
+                        // bits 3..5 must be written to zero 
+                        // when ADCSRB is written.
+  uint8_t acme:1;       // Bit 6 - ACME: Analog Comparator Multiplexor Enable see pg 202
+  uint8_t reserved7:1;  // Bit 7 Reserved
+                        // To ensure compatibility with future devices, 
+                        // bit 7 must be written to zero 
+                        // when ADCSRB is written.
+};
+#define adcsrb (*(volatile regADCSRB *)(&ADCSRB))
+
 struct regACSR {
   // struct concept for accessing bits in register ACSR
   // is thanks to Tom Almy's book, "Far Inside The Arduino"
-  int acis:2; // Bits 1, 0 – ACIS1, ACIS0: Analog Comparator Interrupt Mode Select
-  int acic:1; // Bit 2 – ACIC: Analog Comparator Input Capture Enable
-  int acie:1; // Bit 3 – ACIE: Analog Comparator Interrupt Enable
-  int aci:1; // Bit 4 – ACI: Analog Comparator Interrupt Flag
-  int aco:1; // Bit 5 – ACO: Analog Comparator Output
-  int acbg:1; // Bit 6 – ACBG: Analog Comparator Bandgap Select
-  int acd:1; // Bit 7 – ACD: Analog Comparator Disable
+  uint8_t acis:2; // Bits 1, 0 – ACIS1, ACIS0: Analog Comparator Interrupt Mode Select
+  uint8_t acic:1; // Bit 2 – ACIC: Analog Comparator Input Capture Enable
+  uint8_t acie:1; // Bit 3 – ACIE: Analog Comparator Interrupt Enable
+  uint8_t aci:1; // Bit 4 – ACI: Analog Comparator Interrupt Flag
+  uint8_t aco:1; // Bit 5 – ACO: Analog Comparator Output
+  uint8_t acbg:1; // Bit 6 – ACBG: Analog Comparator Bandgap Select
+  uint8_t acd:1; // Bit 7 – ACD: Analog Comparator Disable
  };
 // How to access the ACSR register as a struct
 // rather than as a byte?
